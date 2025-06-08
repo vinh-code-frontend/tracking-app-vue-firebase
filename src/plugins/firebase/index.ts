05f0a47b3bc639bb834dd80aa2ad89e4fb825b12
Nguyen Le Vinh <vikvenus199x@gmail.com>
2025-06-08 17:14:11 +0700

    update env config

--- src/plugins/firebase/index.ts
+++ src/plugins/firebase/index.ts
@@ -7,7 +7,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
- 
+  apiKey: import.meta.env.VITE_FIREBASE_apiKey,
+  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+  projectId: import.meta.env.VITE_FIREBASE_projectId,
+  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+  appId: import.meta.env.VITE_FIREBASE_appId
 };
 
 // Initialize Firebase
